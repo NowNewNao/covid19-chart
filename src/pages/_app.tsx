d10b4b9f2c7dcf095dd5c5d5ea4c8d02ada3e006
nao <naoyosetani@gmail.com>
2022-04-02 22:06:39 +0900

    Customise theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { AppProps } from 'next/app';
 import React from 'react';
 import '../../styles/globals.css';
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <ThemeProvider attribute="class" defaultTheme="dark">
+        <Component {...pageProps} />
+      </ThemeProvider>
+  );
 }
 
 export default MyApp;
