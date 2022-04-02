70fdb0c628781c916afa801010de790ec6ec5d5f
nao <naoyosetani@gmail.com>
2022-04-02 22:06:59 +0900

    Import the button in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from 'react';
+import { ChangeThemeButton } from '../ChangeThemeButton';
 
 const Header:FC = () => {
-
     return (
         <header>
-            <h1>Covid-19 Tracke</h1>
+            <h1>Covid-19 Tracker</h1>
+            <ChangeThemeButton />
         </header>
     );
 };
