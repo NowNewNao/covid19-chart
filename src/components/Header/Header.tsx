1cdb8a6232e90d6c2a46d1950a7bdfaa2509dda6
nao <naoyosetani@gmail.com>
2022-04-02 18:49:36 +0900

    Prep: Create Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+const Header:FC = () => {
+
+    return (
+        <header>
+            <h1>Covid-19 Tracke</h1>
+        </header>
+    );
+};
+
+
+export {Header};
