4a63980af01196ea46182cc84e4619076a394232
nao <naoyosetani@gmail.com>
2022-04-02 13:51:20 +0900

    Refactoring: Show top page

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { TopPage } from '@/components/TopPage/TopPage';
+import { GetStaticProps } from 'next';
+import { FC } from 'react';
+
+
+
+const Home: FC = () => <TopPage />;
+
+export default Home;
